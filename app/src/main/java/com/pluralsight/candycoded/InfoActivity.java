456b1bc37d178e4c95a3567789f40a51f35ef1a5
tizentos <tizentos@gmail.com>
2019-10-26 19:16:21 -0500

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -20,6 +22,22 @@ public class InfoActivity extends AppCompatActivity {
                 load(uri).
                 into(candyStoreImageView);
 
+
+    }
+    public void createMapIntent(View view){
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) !=  null)
+            startActivity(mapIntent);
+    }
+
+    public  void createPhoneIntent(View view){
+        Intent intentDial = new Intent(Intent.ACTION_DIAL);
+        intentDial.setData(Uri.parse("tel:0123456789"));
+        startActivity(intentDial);
 
     }
 
